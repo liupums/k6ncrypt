97c7351b152fd0e86193e9fb81516d23c2da8ec3
Pop LIU <puliu@microsoft.com>
2022-04-11 10:10:49 -0700

    more cert req

--- createcert.cmd
+++ createcert.cmd
@@ -1,2 +1,2 @@
-openssl req -newkey rsa:2048 -new -nodes -x509 -days 3650 -out cert.pem -keyout key.pem -subj "/C=US/ST=WA/L=Redmond/O=Contoso/OU=security/CN=localhost"  -addext "subjectAltName = DNS:localhost"
+openssl req -config openssl.cnf -newkey rsa:2048 -new -nodes -x509 -days 3650 -out cert.pem -keyout key.pem -extensions v3_req
 openssl pkcs8 -in key.pem -traditional -nocrypt -out rsakey.pem
