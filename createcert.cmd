e0422f2a2c0920360dce67943d3bf0f48f910511
Pop Liu <puliu@microsoft.com>
2022-04-11 08:18:20 -0700

    Update createcert.cmd
    add command to convert pkcs8 to pkcs1
    https://stackoverflow.com/questions/48958304/pkcs1-and-pkcs8-format-for-rsa-private-key

--- createcert.cmd
+++ createcert.cmd
@@ -1,1 +1,2 @@
 openssl req -newkey rsa:2048 -new -nodes -x509 -days 3650 -out cert.pem -keyout key.pem -subj "/C=US/ST=WA/L=Redmond/O=Contoso/OU=security/CN=localhost"  -addext "subjectAltName = DNS:localhost"
+openssl pkcs8 -in key.pem -traditional -nocrypt -out rsakey.pem
