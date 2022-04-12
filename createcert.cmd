8c4024f485c78349cdc285950d2c81ceb1f93af5
liupums <puliu@microsoft.com>
2022-04-11 22:34:04 -0700

    generate pfx which can be installed to cert store

--- createcert.cmd
+++ createcert.cmd
@@ -1,2 +1,3 @@
 openssl req -config openssl.cnf -newkey rsa:2048 -new -nodes -x509 -days 3650 -out cert.pem -keyout key.pem -extensions v3_req
 openssl pkcs8 -in key.pem -traditional -nocrypt -out rsakey.pem
+openssl pkcs12 -export -out cert.pfx -inkey key.pem -in cert.pem
